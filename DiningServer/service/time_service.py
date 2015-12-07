9f811c8197caf5d06c50392d7a07d27b70e8c0df
xiang <xiang9311@sina.cn>
2015-12-07 14:33:14 +0800

    时间选择器中的时间定义  设置已选商品列表

--- DiningServer/service/time_service.py
+++ DiningServer/service/time_service.py
@@ -5,11 +5,28 @@ from DiningServer.common.time_format_util import HOUR_AND_MINUTE
 import time
 
 """
-获取时间选项 返回 “立即送达”
+获取时间选项 
 """
 def getTimeOption():
-    time_list = ['立即送出']
+    time_list = []
     minute = time.localtime(time.time())
     stemp_now = time.time()
-    time_list.extend([time.strftime(HOUR_AND_MINUTE, time.localtime(stemp_now + i * 900)) for i in range(2,6)])
+    local_time = time.strftime(HOUR_AND_MINUTE, time.localtime(stemp_now))
+    l_hour , l_minute = [int(i) for i in local_time.split(':')]
+    for i in range(15):
+        if not l_minute % 15:
+            break
+        else:
+            l_minute += 1
+            if l_minute >= 60:
+                l_minute = 0
+                l_hour += 1
+    for i in range(5):
+        time_list.append('%02d:%02d' % (l_hour, l_minute))
+        # 往后加15分钟
+        l_minute += 15
+        if l_minute >= 60:
+                l_minute = 0
+                l_hour += 1
+
     return time_list
